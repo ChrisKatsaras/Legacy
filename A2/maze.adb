e19992affb3e2de6d9cf46c83f5c16a397416dd6
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-02-05 20:59:43 -0500

    Implemented stack and now working on traversing through maze

--- A2/maze.adb
+++ A2/maze.adb
@@ -2,8 +2,10 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.integer_text_IO; use Ada.integer_text_IO;
 with Ada.strings.unbounded; use ada.strings.unbounded;
 with Ada.strings.unbounded.Text_IO; use ada.strings.unbounded.Text_IO;
-
+with stack; use stack;
 procedure Maze is
+	
+  	
 
    infp : file_type;
    filename : unbounded_string;
@@ -12,6 +14,10 @@ procedure Maze is
    width : integer := 0;
    scanChar : character;
    maze : array(1..50,1..50) of character;
+   currentX : integer;
+   currentY : integer;
+   currentSymbol : character;
+	
 begin
 
    Put_Line("Input filename");
@@ -19,15 +25,25 @@ begin
    open(infp,in_file,"maze.txt");--Gotta fix this to work with filename
    get(infp,length);
    get(infp,width);
-   
+
    --Scans maze from file into 2D array that holds maze values
    for i in 1..length loop
    		for j in 1..width loop
    			get(infp,scanChar);
    			maze(i,j) := scanChar;
+   			if(scanChar = 'e') then
+   				currentX := i;
+   				currentY := j;
+   				currentSymbol := 'e';
+   			end if; 
    		end loop;
    end loop;
-  
 
+   push('x',2,3);
+   push('*',10,2);	
+   pop(currentSymbol,currentX,currentY);
+   print;
    close(infp);
+
 end Maze;
+
