4d83ccafba328cc3986db614bc615056dee189e0
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-03-07 13:27:36 -0500

    Added filename input

--- A3/text.cob
+++ A3/text.cob
@@ -4,7 +4,7 @@ program-id. A3text.
 environment division.
 input-output section.
 file-control.
-select textFile assign to "LOTR.txt"
+select textFile assign to file-name
 organization is line sequential.
 
 data division.
@@ -15,14 +15,24 @@ FD textFile.
 working-storage section.
 01 ok	pic x(1000).
 77 eof-switch pic 9 value 1.
+01 file-name  pic x(50).
 
 procedure division.
-
+	
+	display "Please input the file you wish to analyise"
+	accept file-name
     open input textFile.
-    read textFile 
-    	at end move zero to eof-switch
-    	not at end move sample-input to ok
-    end-read.	
-    display ok
+    
+    *>perform loop until eof-switch = 0.
+	    read textFile 
+	    	at end 
+	    		move zero to eof-switch
+	    	not at end 
+	    		move sample-input to ok
+	    	display ok
+	    end-read.	
+    *>loop.
+
 
     close textFile.
+stop run.
