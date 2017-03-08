4fb5ee1dbad8173f7e4c3776a9721a5c05e538bd
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-03-08 16:21:49 -0500

    Word and sentence count works for vader.txt

--- A3/text.cob
+++ A3/text.cob
@@ -1,3 +1,8 @@
+*>A3: Text analyizer
+*>By: Christopher Katsaras
+*>Due: March 24th/17
+*>This program analyizes a textfile of the user's choice
+
 identification division.
 program-id. A3text.
 
@@ -22,7 +27,7 @@ working-storage section.
 01 line-length pic 9(10).
 01 i pic 9(10).
 77 eof-switch pic 9 value 1.
-77 word-flag  pic 8 value 0.
+77 word-flag  pic 9 value 0.
 
 procedure division.
 	
@@ -44,25 +49,39 @@ procedure division.
 	    	not at end
 	    		move zero to trim-count
 	    		move input-text to storage
+	    		*> Flips the string and counts how many spaces there are at the beginning
+	    		*> This allows me to disregard trailing spaces
 	    		INSPECT function reverse(storage) tallying trim-count for leading spaces
 
 				COMPUTE line-length = LENGTH OF storage - trim-count
 				display storage(1:line-length)
 				*>display line-length
-
+				move zero to word-flag
 				perform varying i from 1 by 1 until i > line-length
 					if storage(i:1) is not = " " then 
 						add 1 to num-chars
-						if storage(i:1) is alphabetic
+						if storage(i:1) is alphabetic and word-flag is zero
+							move 1 to word-flag
+							add 1 to num-words
 
+						else if storage(i:1) is = "." or storage(i:1) is = "?" or storage(i:1) is = "!"  
+						    add 1 to num-sentences
+						    move zero to word-flag		
+						end-if	
+							
 					else 
 						move zero to word-flag		
-					end-if 
+					end-if
 
 				end-perform
 	    end-read	
 	end-perform
+	display "Number of chars"
 	display num-chars
+	display "Number of words"
+	display num-words
+	display "Number of sentences"
+	display num-sentences
 
     close textFile.
 stop run.
