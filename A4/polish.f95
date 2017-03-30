aa73a9bc03c038babf47c92da9ddc8c368b49e6b
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-03-30 16:26:30 -0400

    Added looping mechanism which allows user to convert more than one equation

--- A4/polish.f95
+++ A4/polish.f95
@@ -16,62 +16,73 @@ program polish
 
 character (len = 40) :: originalString, polishString, stack
 integer :: answerLength, i, polishLength, top, test, priority
+character :: playFlag
 
-polishLength = 1
-top = 0
-stack(top:top) = '%'
- 
-write(*,*) "Please input an algebraic expression to convert"
-read(*,*) originalString
-answerLength = len_trim(originalString)
 
-do i=1,answerLength
-    select case (originalString(i:i))
-        case ('%','+','-','*','/','^')!,'(',')')
-            do
-                if (priority(originalString(i:i)) > priority(stack(top:top))) then
-                    top = top + 1
-                    stack(top:top) = originalString(i:i)
-                    exit
-                else 
-                    polishLength = polishLength + 1
-                    polishString(polishLength:polishLength) = stack(top:top)
-                    top = top - 1               
-                endif
-            end do
-        case ('(',')')
-            if(originalString(i:i) == '(') then
-                top = top + 1
-                stack(top:top) = '('
-            else if(originalString(i:i) == ')') then
-                do while(priority(stack(top:top)) /= priority('('))
-                    polishLength = polishLength + 1
-                    polishString(polishLength:polishLength) = stack(top:top)
-                    top = top - 1
+playFlag = 'Y'
+
+do while(playFlag == 'Y')
+    polishLength = 1
+    top = 0
+    stack(top:top) = '%'
+    write(*,*) "Please input an algebraic expression to convert"
+    read(*,*) originalString
+    answerLength = len_trim(originalString)
+
+    do i=1,answerLength
+        select case (originalString(i:i))
+            case ('%','+','-','*','/','^')!,'(',')')
+                do
+                    if (priority(originalString(i:i)) > priority(stack(top:top))) then
+                        top = top + 1
+                        stack(top:top) = originalString(i:i)
+                        exit
+                    else 
+                        polishLength = polishLength + 1
+                        polishString(polishLength:polishLength) = stack(top:top)
+                        top = top - 1               
+                    endif
                 end do
-                top = top - 1
-            end if
-        case ('a':'z','A':'Z','0':'9')
+            case ('(',')')
+                if(originalString(i:i) == '(') then
+                    top = top + 1
+                    stack(top:top) = '('
+                else if(originalString(i:i) == ')') then
+                    do while(priority(stack(top:top)) /= priority('('))
+                        polishLength = polishLength + 1
+                        polishString(polishLength:polishLength) = stack(top:top)
+                        top = top - 1
+                    end do
+                    top = top - 1
+                end if
+            case ('a':'z','A':'Z','0':'9')
+                polishLength = polishLength + 1
+                polishString(polishLength:polishLength) = originalString(i:i)
+            case default
+                write(*,*) "Invalid operator!"        
+        end select
+
+    end do
+
+    do
+        if(top <= 0) then
+            exit
+        end if
+        if(stack(top:top) == '(') then
+            write(*,*) "Unmatched bracket"    
+        else
             polishLength = polishLength + 1
-            polishString(polishLength:polishLength) = originalString(i:i)
-        case default
-            write(*,*) "Invalid operator!"        
-    end select
+            polishString(polishLength:polishLength) = stack(top:top)    
+        end if
+        top = top - 1 
+    end do
+
+    write(*,*) polishString(1:polishLength);
+
+    write(*,*) 'Would you like to convert another expression? (Type Y to play again or any other key to exit)'
+    read(*,*) playFlag !Prompts the user if they would like to convert another expression
 
 end do
-
-do
-    if(top <= 0) then
-        exit
-    end if
-    polishLength = polishLength + 1
-    polishString(polishLength:polishLength) = stack(top:top)
-    top = top - 1 
-end do
-
-write(*,*) polishString(1:polishLength);
-
-
 end
 
 integer function priority(sym)
