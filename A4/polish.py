fe195fc2e8a17a9eea20eeab1fdba44e050a5daa
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-04-02 14:45:22 -0400

    Adding python file

---
+++ A4/polish.py
@@ -0,0 +1,22 @@
+#A4: Reverse polish form: Python
+#Written by Christopher Katsaras
+#April 7th/2017
+#
+#***********
+#Compile/run
+#***********
+#Type "make python"
+#
+#*****************
+#Known limitations
+#*****************
+#
+#
+
+originalString = []
+
+
+print "Please input an algebraic expression to convert"
+
+originalString = raw_input()
+length = len(originalString)
